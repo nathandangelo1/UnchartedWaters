c0e243abcdd27192e0431fae8442da1a75147328
Nate D. D'Angelo <69875151+nathandangelo1@users.noreply.github.com>
2022-12-17 10:05:43 -0600

    first commit

---
+++ Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnchartedWaters_CSHARP {
+    internal class Car {
+        public string PetName { get; set; }
+        public string Make { get; set; }
+    }
+}
